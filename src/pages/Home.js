63cfe2531f36d1828f45eb67317f9e8045a7ca2d
malapashish <malapashish10@gmail.com>
2021-08-07 01:18:18 +0530

    Added data file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react"; 
 
 const Home = () => {
-    return (
-        <div className="homeImage">
-            Hi from Home
-        </div>
-    )
-}
+  return (
+    <div className="homeImage">
+      Hi from Home 
+    </div>
+  );
+};
 
 export default Home;
