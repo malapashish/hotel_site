ec34f6a2c3943d2af344c27788f8be44dd29b168
malapashish <malapashish10@gmail.com>
2021-08-07 14:32:45 +0530

    Some minor merge error solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Home from './Pages/Home'
-import Room from "./Pages/Room";
-import Error from "./Pages/Error";
-import Contact from "./Pages/Contact";
+import Home from './pages/Home'
+import Room from "./pages/Room";
+import Error from "./pages/Error";
+import Contact from "./pages/Contact";
 import { Navbar } from "./components/Navbar";
 
 export default function App() {
