073e3b587ab086b334423e2a000deee92e80df08
malapashish <malapashish10@gmail.com>
2021-08-08 19:46:35 +0530

    Path changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Home from './pages/Home'
-import Room from "./pages/Room";
-import Error from "./pages/Error";
-import Contact from "./pages/Contact"; 
+import Home from './Pages/Home'
+import Room from "./Pages/Room";
+import Error from "./Pages/Error";
+import Contact from "./Pages/Contact"; 
 import SingleRoom from "./Pages/SingleRoom"; 
 import { Navbar } from "./components/Navbar";
 
